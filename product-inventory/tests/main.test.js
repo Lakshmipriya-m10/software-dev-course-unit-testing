204e523019eebe305496eb0b478e6aba99323e27
pujajathar <pujajathar91@.com>
2026-06-08 14:56:26 -0500

    worked on a logic in main.js file

---
+++ product-inventory/tests/main.test.js
@@ -0,0 +1,29 @@
+
+const  { calculateDiscount, filterProducts, sortInventory } = require('../main.js');
+
+test("applies a valid discount rate", () => {
+    expect(calculateDiscount(100, 0.1)).toBe(90);
+});
+
+test("handles an invalid discount rate gracefully", () => {
+    expect(calculateDiscount(100, -0.1)).toBe(null);
+});
+
+test("handles edge case with price of 0", () => {
+    expect(calculateDiscount(0, 0.2)).toBe(0);
+});
+
+// task 2
+test("Filtered array will be", () => {
+    expect(filterProducts([{name: laptop, price: 500}], )).toBe(90);
+});
+
+test("handles an invalid discount rate gracefully", () => {
+    expect(calculateDiscount(100, -0.1)).toBe(null);
+});
+
+test("handles edge case with price of 0", () => {
+    expect(calculateDiscount(0, 0.2)).toBe(0);
+});
+
+
