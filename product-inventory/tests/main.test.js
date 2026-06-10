4a205be65843ab64fdd5f256d4106d128f9ab102
Lakshmipriya-m10 <lakshmipriyaark@gmail.com>
2026-06-09 20:37:27 -0500

    Update main.test.js
    I updated task 2 test cases

--- product-inventory/tests/main.test.js
+++ product-inventory/tests/main.test.js
@@ -13,19 +13,33 @@ test("handles edge case with price of 0", () => {
     expect(calculateDiscount(0, 0.2)).toBe(0);
 });
 
-// task 2
-test("Filtered array will be", () => {
-    expect(filterProducts([{name: "laptop", price: 500}],
-        product => product.price > 100 )).toEqual([{name: "laptop", price: 500}]
-    );
+test("Valid Filtered array will be", () => {
+ 
+const productPrice = [20, 30, 40, 60, 80, 100, 450];
+
+const highPrice = filterProducts(productPrice, price => price > 100);
+
+expect(highPrice[0]).toBe(450);
+
 });
 
-test("handles an invalid discount rate gracefully", () => {
-    expect(calculateDiscount(100, -0.1)).toBe(null);
+test("invalid case with no item found", () => {
+ 
+const productPrice = [20, 30, -40, 60, 80];
+
+const highPrice = filterProducts(productPrice, price => price < 0);
+
+expect(highPrice[0]).toBe(-40);
+
 });
 
-test("handles edge case with price of 0", () => {
-    expect(calculateDiscount(0, 0.2)).toBe(0);
-});
+test("handles edge case with no item found", () => {
+ 
+const productPrice = ["apple"];
 
+const highPrice = filterProducts(productPrice, price => price > 200);
+
+expect(highPrice[0]).toBe(undefined);
+
+});
 
