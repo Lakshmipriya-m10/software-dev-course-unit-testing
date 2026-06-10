d8f818ec744561c557d7e0bb896b665d4f6d2e4b
Lakshmipriya-m10 <lakshmipriyaark@gmail.com>
2026-06-09 20:49:05 -0500

    Update main.test.js
    task 3 test case updated

--- product-inventory/tests/main.test.js
+++ product-inventory/tests/main.test.js
@@ -1,8 +1,9 @@
 
-const  { calculateDiscount, filterProducts, sortInventory } = require('../main.js');
+const { calculateDiscount, sortInventory,filterProducts } = require("../main.js");
 
+//Task 1:calculateDiscount -positive, negative, and edge cases.
 test("applies a valid discount rate", () => {
-    expect(calculateDiscount(100, 0.1)).toBe(90);
+    expect(calculateDiscount(100,0.10)).toBe(90);
 });
 
 test("handles an invalid discount rate gracefully", () => {
@@ -13,6 +14,8 @@ test("handles edge case with price of 0", () => {
     expect(calculateDiscount(0, 0.2)).toBe(0);
 });
 
+
+// task 2 -filterProducts -positive, negative, and edge cases.
 test("Valid Filtered array will be", () => {
  
 const productPrice = [20, 30, 40, 60, 80, 100, 450];
@@ -43,3 +46,30 @@ expect(highPrice[0]).toBe(undefined);
 
 });
 
+// task 3 - sortInventory -positive, negative, and edge cases.
+const inventory = [
+    { name: "bella", category: "project Developer" },
+    { name: "ani", category: "frontEnd Developer" },
+    { name: "nancy", category: "dataAnalyst" }]
+
+test("Sort inventory - test valid case", () => {
+    console.log(sortInventory(inventory, "name"));
+    expect(sortInventory(inventory, "name")).toEqual
+    ([
+        { name: "ani", category: "frontEnd Developer" },
+        { name: "bella", category: "project Developer" },
+        { name: "nancy", category: "dataAnalyst" }
+    ]);
+});
+
+
+test("Sort inventory - test invalid case", () => {
+    console.log(sortInventory(inventory, "age"));
+    expect(sortInventory(inventory, "name")).toBe(0);
+});
+    
+test("Sort inventory - test Edge case", () => {
+    console.log(sortInventory(inventory, ""));
+    expect(sortInventory(inventory, "name")).toBe(0);
+});
+    
